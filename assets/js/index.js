bbbe596835891ea4704a4d1ff72dfbb1a3961ac1
SerjAyvazyan-2000 <ayvazyanserj3@gmail.com>
2025-05-05 22:24:32 +0300

    new

--- assets/js/index.js
+++ assets/js/index.js
@@ -103,39 +103,44 @@ document.addEventListener('DOMContentLoaded', function () {
         observer.observe(el);
     });
 });
+const mySwiper = document.querySelector('.mySwiper')
+
+if(mySwiper){
+    const swiper = new Swiper(".mySwiper", {
+        spaceBetween: 20,
+        slidesPerView: 3,
+
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+        breakpoints: {
+            320: {
+                slidesPerView: 1,
+                spaceBetween: 20,
+            },
+            500: {
+                slidesPerView: 1,
+                spaceBetween: 20,
+            },
+            768: {
+                slidesPerView: 2,
+                spaceBetween: 30,
+            },
+            1024: {
+                slidesPerView: 3,
+                spaceBetween: 20,
+            },
+        },
+    });
+
+}
 
 
-const swiper = new Swiper(".mySwiper", {
-    spaceBetween: 20,
-    slidesPerView: 3,
-
-    pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-    },
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-    },
-    breakpoints: {
-        320: {
-            slidesPerView: 1,
-            spaceBetween: 20,
-        },
-        500: {
-            slidesPerView: 1,
-            spaceBetween: 20,
-        },
-        768: {
-            slidesPerView: 2,
-            spaceBetween: 30,
-        },
-        1024: {
-            slidesPerView: 3,
-            spaceBetween: 20,
-        },
-    },
-});
 
 const observer = new IntersectionObserver(entries => {
     entries.forEach(entry => {
